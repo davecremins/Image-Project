1cd4f39b236c918db22597b76e7ff07390476246
unknown <dave.cremins@dev-lenovo-dc.kneat.org>
2015-09-23 00:14:38 +0100

    Gulp does bower install and moves dependencies into dist folder

--- Server/gulpfile.js
+++ Server/gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp');
 var bower = require('gulp-bower');
+var clean = require('gulp-clean');
  
 gulp.task('bower', function() {
   return bower()
-    .pipe(gulp.dest('public/'));
+      .pipe(gulp.dest('dist/public/'));
 });
 
-gulp.task('default', ['bower']);
+gulp.task('clean-bower-components', ['bower'], function (cb) {
+  gulp.src('bower_components', {read: false})
+    .pipe(clean());
+});
+ 
+
+gulp.task('default', ['clean-bower-components']);
