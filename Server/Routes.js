9c36fb5143817fa0579e8f65a41dad10dadfb3a0
Dave <davecremins@gmail.com>
2015-09-23 00:32:33 +0100

    Removed unused code

--- Server/Routes.js
+++ Server/Routes.js
@@ -6,19 +6,4 @@ module.exports = function (app) {
    app.get('/', function (req, res) {
       res.sendFile(path.join(__dirname+'/views/ImageTagger.html'));
    });
-
-   app.get('/reportdata', function (req, res) {
-
-      console.log('Request recieved for load data.');
-
-      fs.readFile('../LoadData.json', 'utf8', function (err, data) {
-         if (err) {
-            console.error('ERROR occured reading load data file.');
-            res.status(500).json({ error: 'Unable to load report data.' });
-         } else {
-            console.log('Load data successfully read.');
-            res.json(JSON.parse(data));
-         }
-      });
-   });
 };
