cda93201a5655d20358c00914d1fa24233697b53
unknown <dave.cremins@dev-lenovo-dc.kneat.org>
2015-09-24 01:09:15 +0100

    Submit multipart form to node and save to disk

--- Server/Routes.js
+++ Server/Routes.js
@@ -1,14 +1,17 @@
 // globals require
 ﻿var path = require('path');
 var fs = require('fs');
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
 module.exports = function (app) {
    app.get('/', function (req, res) {
       res.sendFile(path.join(__dirname+'/views/ImageTagger.html'));
    });
 
-   app.post('/UploadImage', function (req, res) {
-      console.log("Received post to UploadImage endpoint");
+   app.post('/upload', upload.single('imageSelection'), function (req, res) {
+      console.log(req.file);
+      console.log(req.body);
       res.status(204).end();
-   });
+   })
 };
