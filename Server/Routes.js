9d28b9dacbf8888f4640cd0f3309f67bbb7a6cd1
unknown <dave.cremins@dev-lenovo-dc.kneat.org>
2015-10-06 22:17:20 +0100

    Capture natural dimensions & new route for next image

--- Server/Routes.js
+++ Server/Routes.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var multer  = require('multer');
 var upload = multer({ dest: 'uploads/' });
 var db = require('./db/db.js')('mongodb://localhost:27017/Images');
+var tagQuery = 'family';
 
 module.exports = function (app) {
    app.get('/', function (req, res) {
@@ -14,13 +15,27 @@ module.exports = function (app) {
       res.sendFile(path.join(__dirname+'/views/ImageTagger.html'));
    });
 
+   app.get('/NextImage', function(req, res){
+
+      // default to first image with tag 'family'
+      db.get('Metadata', 'tags', 'family', function(result){
+         console.log(result[0]);
+         res.sendFile(path.join(__dirname + '/' + result[0].file.path));
+      });
+
+   });
+
    app.post('/upload', upload.single('imageSelection'), function (req, res) {
       console.log(req.file);
       console.log(req.body);
 
       var imgData = {
          file: req.file,
-         tags: req.body.tags // Convert this to array later
+         tags: req.body.tags, // Convert this to array later
+         originalDimensions: {
+            width: parseInt(req.body.origWidth),
+            height: parseInt(req.body.origHeight)
+         }
       };
 
       db.insert('Metadata', imgData);
--- Server/dist/preview.js
+++ Server/dist/preview.js
@@ -2,13 +2,22 @@ function readURL(input) {
    if (input.files && input.files[0]) {
       var reader = new FileReader();
       reader.onload = function(e) {
-         $('#imageToTag').attr('src', e.target.result);
+         $('#imageToTag').attr('src', e.target.result).load(function(){
+            captureOriginalDimensions({
+               width: this.naturalWidth, 
+               height: this.naturalHeight
+            });
+         });
       }
 
       reader.readAsDataURL(input.files[0]);
+      var captureOriginalDimensions = function(dimensions){
+         $('#origWidth').val(dimensions.width);
+         $('#origHeight').val(dimensions.height);
+      };
    }
 }
 
-$("#imageSelector").change(function() {
+$('#imageSelector').change(function() {
    readURL(this);
 });
