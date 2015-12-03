4b315beed772980c8df44ad3fb9aa5941051c0e2
Dave Cremins <dave.cremins@dev-lenovo-dc.kneat.org>
2015-12-03 22:25:04 +0000

    Updated file structure

--- Server/README.md
+++
@@ -1,5 +0,0 @@
-# Image Tagger
-Select local image, preview & tag it
-
-## Next
-Setup simple NodeJS webserver that accepts image & tags and stores them in MongoDB  
--- Server/configure.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = express();
-
-module.exports = function(){
-   app.use(express.static(__dirname + '/dist'));
-   return app;
-};
