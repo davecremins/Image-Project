8dac1dcc21098d53ebd6d5379d0ac0e8b193eaea
Dave Cremins <davecremins@gmail.com>
2016-02-02 22:25:59 +0000

    Adding tests for imageIndexer

--- test/test.js
+++ test/test.js
@@ -1,9 +1,41 @@
+/* globals describe, it, beforeEach, imageIndexer */
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
-  });
+
+describe('Image Indexer', function () {
+   
+   describe('current is null', function () {
+      var imageIndexer = require('../lib/imageIndexer');
+      it('should return null when imageIndexer is required', function () {
+         assert.equal(null, imageIndexer.current());
+      });
+   });
+   
+   describe('set collection ', function () {
+      var imageIndexer = require('../lib/imageIndexer');
+      it('should return correct size after set operation', function () {
+         var data = [{id: 1, name: 'bob'}, {id: 4, name: 'ray'}];
+         imageIndexer.set(data);
+         assert.equal(data.length, imageIndexer.size());
+      });
+   });
+   
+   describe('collection modifications', function () {
+      var imageIndexer = require('../lib/imageIndexer');
+      var data = [{id: 1, name: 'bob'}, {id: 4, name: 'ray'}];
+      
+      beforeEach(
+         function () {
+            imageIndexer.set(data);
+         }
+      );
+      
+      it('current should return non-null object after collection initialization', function () {
+         assert(imageIndexer.current() != null);
+      });
+      
+      it('next should return non-null object after collection initialization', function () {
+         assert(imageIndexer.next() != null);
+      });
+   });
+   
 });
